105bf2813a1913743d0dd8ce4fbfa8b0f346f4cd
Silja Ye-Chi Sandersen <silja.sandersen@hotmail.com>
2023-02-26 15:30:02 +0100

    Initial commit with gum
    Converts input to gum for a more interactive experience
    
    Co-authored-by: Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>

--- pkgs.sh
+++ pkgs.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-read -p "Which package do you want to install? " pkgname
-read -p "Should $pkgname be installed from source (s) og using a package manager (p)? [P|s] " pkgsource
+# Take input from user
+pkgname=$(gum input --prompt "Packagename > " --placeholder "Which package do you want to install? ")
+
+# Prompt user to choose
+echo "Should $pkgname be installed from source or using a package manager?"
+pkgsource=$(gum choose --limit=1 "Source" "Package manager")
+
+echo $pkgsource
+
+
+
+#read -p "Should $pkgname be installed from source (s) og using a package manager (p)? [P|s] " pkgsource
 
 # Ensure that that it is always lower case and that the variable is always set
-pkgsource=$(echo ${pkgsource:-p} | awk '{print tolower($0)}')
-read -p "The URL for the package: " pkguri
+#pkgsource=$(echo ${pkgsource:-p} | awk '{print tolower($0)}')
+#read -p "The URL for the package: " pkguri
 
 # Check the permissions for /usr/local/src
-usrpermbits=$(stat -c "%a" /usr/local/src)
+#usrpermbits=$(stat -c "%a" /usr/local/src)
